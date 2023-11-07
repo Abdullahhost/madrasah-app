a3097c98c46af6e903e8148d918132124602a108
Abdullah Al Mamun <abdullahgodboy@gmail.com>
2023-11-07 13:59:46 +0600

    in worker section set dark mode style

--- front-end/src/features/pages_omponent/worker/index.jsx
+++ front-end/src/features/pages_omponent/worker/index.jsx
@@ -1,32 +1,79 @@
 
+import { useSelector } from "react-redux"
 
 import useFetch from "../../../settings/hooks"
 import Skeleton from "../../skeloten"
 
+
+
 const Worker = () => {
 
-    const {data, loading} = useFetch('https://madrasah-app.onrender.com/worker')
-   
+  const { data, loading } = useFetch('https://madrasah-app.onrender.com/worker')
+
+  const lightDarkMode = useSelector((state) => state.dark.dark)
+
   return (
     <>
-      {loading ? <Skeleton /> : <div className="card_ui_section">
-      {data?.map((item) => {
-          return <div className='card_ui' key={item._id}>
-             <div className='pathdeasign'></div>
-            <div className="card_image">
-            <img src={item.profile} alt="Worker Image" />
+      {loading ?
+        <Skeleton /> :
+        <div
+
+          className="card_ui_section">
+
+          {data?.map((item) => {
+
+            return <div className='card_ui'
+              style={lightDarkMode
+                ?
+                {
+                  boxShadow: "0 0 5px #fff",
+                  border: '1px solid #fff1'
+                }
+                :
+                {
+                  boxShadow: "0 0 5px rgba(59, 59, 59, 0.467)",
+                  border: "1px solid #0001"
+                }
+              }
+
+              key={item._id}
+
+            >
+
+              <div className='pathdeasign'
+                style={
+                  lightDarkMode
+                    ?
+                    { background: "radial-gradient(rgb(255, 255, 100) 10%, rgb(201, 255, 180) 30%, rgb(0, 0, 0))" }
+
+                    :
+                    { background: 'radial-gradient(rgb(251, 255, 0) 20%, rgb(201, 255, 180), rgb(255, 255, 255))' }
+                }
+
+              ></div>
+              <div className="card_image">
+                <img src={item.profile} alt="Worker Image" />
+              </div>
+              <div className="card_content"
+
+                style={lightDarkMode ?
+
+                  { background: "linear-gradient(to top,  #333 ,#0002)", color: "#fff" }
+                  :
+                  { background: "linear-gradient(to top, #0002, #fff)" }
+                }
+
+              >
+                <h2>{item.workerName}</h2>
+                <small><strong>{item.designation} </strong> Shahdharipara K.A.S Alim Madrashah</small><br />
+                <p>Depertment: <strong> {item.depertment} </strong></p>
+                <p>Educational Qualification : {item.educationQualification}</p>
+                <cite>Phone Number : {item.mobileNumber}</cite><br />
+                <small>Email :{item.emailAddress}</small>
+              </div>
             </div>
-            <div className="card_content">
-              <h2>{item.workerName}</h2>
-              <small><strong>{item.designation} </strong> Shahdharipara K.A.S Alim Madrashah</small><br />
-              <p>Depertment: <strong> {item.depertment} </strong></p>
-              <p>Educational Qualification : {item.educationQualification}</p>
-              <cite>Phone Number : {item.mobileNumber}</cite><br />
-              <small>Email :{item.emailAddress}</small>
-            </div>
-          </div>
-        })}
-      </div> }
+          })}
+        </div>}
     </>
   )
 }
