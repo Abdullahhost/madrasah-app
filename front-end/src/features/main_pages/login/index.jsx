b2c8cf283a5b5b1d2246742aec6918bd6cc0ba7b
Abdullah Al Mamun <abdullahgodboy@gmail.com>
2024-05-22 21:22:53 +0600

    reload fix

--- front-end/src/features/main_pages/login/index.jsx
+++ front-end/src/features/main_pages/login/index.jsx
@@ -1,107 +1,112 @@
+import { useState } from "react";
 
-import { useState } from 'react'
+import "./index.css";
 
-import './index.css'
-
-import { useDispatch, useSelector } from 'react-redux'
-import { authSliceActions } from '../../../settings/redux/slice/auth';
-import axios from 'axios';
-import { Link, NavLink, useNavigate } from 'react-router-dom';
-import { Skeleton } from '@mui/material';
-
-
+import { useDispatch, useSelector } from "react-redux";
+import { authSliceActions } from "../../../settings/redux/slice/auth";
+import axios from "axios";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+// import { Skeleton } from '@mui/material';
+import { Skeleton } from "../../skeloten";
 
 const Login_page = () => {
+  const dispatch = useDispatch();
 
+  const navigate = useNavigate();
 
-    const dispatch = useDispatch();
+  const [userInfo, setUserInfo] = useState({
+    userName: undefined,
+    password: undefined,
+  });
 
-    const navigate = useNavigate();
+  const handleChange = (e) => {
+    setUserInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
+  const handleClick = async () => {
+    dispatch(authSliceActions.loginStart());
 
+    try {
+      const res = await axios.post(
+        "https://madrasah-app.onrender.com/auth/login",
+        userInfo,
+        { withCredentials: true }
+      );
+      dispatch(authSliceActions.loginSuccess(res.data));
 
-    const [userInfo, setUserInfo] = useState({
-        userName: undefined,
-        password: undefined
-
-    })
-
-    const handleChange = (e) => {
-        setUserInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }))
-
+      navigate("/");
+      window.location.reload();
+    } catch (err) {
+      dispatch(authSliceActions.loginFailer(err.response.data));
     }
+  };
+  const getData = useSelector((state) => state.auth);
 
+  return (
+    <div>
+      {getData.loading ? (
+        <Skeleton />
+      ) : (
+        <section className="login_section">
+          <div className="box">
+            <div className="square" style={{ "--i": 0 }}></div>
+            <div className="square" style={{ "--i": 1 }}></div>
+            <div className="square" style={{ "--i": 2 }}></div>
+            <div className="square" style={{ "--i": 3 }}></div>
+            <div className="square" style={{ "--i": 4 }}></div>
 
-
-    const handleClick = async () => {
-
-        dispatch(authSliceActions.loginStart())
-
-        try {
-            const res = await axios.post('https://madrasah-app.onrender.com/auth/login', userInfo, { withCredentials: true })
-            dispatch(authSliceActions.loginSuccess(res.data));
-
-            
-            
-            navigate('/');
-            window.location.reload();
-
-        } catch(err) {
-            dispatch(authSliceActions.loginFailer(err.response.data))
-        }
-    }
-    const getData = useSelector((state) => state.auth);
-    
-    return (
-        <div>
-            {getData.loading ? <Skeleton /> : <section className='login_section'>
-
-
-                <div className="box">
-
-                    <div className="square" style={{ "--i": 0 }}></div>
-                    <div className="square" style={{ "--i": 1 }}></div>
-                    <div className="square" style={{ "--i": 2 }}></div>
-                    <div className="square" style={{ "--i": 3 }}></div>
-                    <div className="square" style={{ "--i": 4 }}></div>
-
-                    <div className="container">
-                        <div className="form">
-                            <h2>Login Forms</h2>
-                            <div className="inputBox">
-
-                                <input type="text" placeholder="User Name" name="userName" onChange={handleChange} required />
-
-                            </div>
-
-                            <div className="inputBox">
-                                <input type="password" placeholder="Password"   pattern=".{8,}" title="Eight or more characters" name="password" onChange={handleChange} />
-                            </div>
-
-                            <div className="inputBox">
-                                <button onClick={handleClick} className='signUp'>
-                                    Login
-                                </button>
-                            </div>
-
-                    
-                 
-
-                            <p className="forget" style={{marginBottom: '.6rem', color: '#555'}}>Continue without login or sign up as a Gest mode ? <NavLink to={'/'}>Click here</NavLink></p>
-                            <p className="forget">Dont Have not an account ? <Link to={'/signup'}>Sign up</Link></p>
-                        </div>
-                    </div>
-
+            <div className="container">
+              <div className="form">
+                <h2>Login Forms</h2>
+                <div className="inputBox">
+                  <input
+                    type="text"
+                    placeholder="User Name"
+                    name="userName"
+                    onChange={handleChange}
+                    required
+                  />
                 </div>
-                            <div className='alertBox'>
-                                { getData.error && <h1 style={{color: "#fff"}}>{getData.error.message}</h1>}
-                            </div>
-            </section>
 
-            }
+                <div className="inputBox">
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    pattern=".{8,}"
+                    title="Eight or more characters"
+                    name="password"
+                    onChange={handleChange}
+                  />
+                </div>
 
-        </div>
-    )
-}
+                <div className="inputBox">
+                  <button onClick={handleClick} className="signUp">
+                    Login
+                  </button>
+                </div>
 
-export default Login_page
+                <p
+                  className="forget"
+                  style={{ marginBottom: ".6rem", color: "#555" }}
+                >
+                  Continue without login or sign up as a Gest mode ?{" "}
+                  <NavLink to={"/"}>Click here</NavLink>
+                </p>
+                <p className="forget">
+                  Dont Have not an account ? <Link to={"/signup"}>Sign up</Link>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="alertBox">
+            {getData.error && (
+              <h1 style={{ color: "#fff" }}>{getData.error.message}</h1>
+            )}
+          </div>
+        </section>
+      )}
+    </div>
+  );
+};
+
+export default Login_page;
